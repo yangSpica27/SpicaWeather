302999b87b531966ef7e2949d5c7f43ba368ec07
spica27 <1286939382@qq.com>
2022-02-19 22:42:32 +0800

    # api 隔离

--- app/src/main/java/me/spica/weather/repository/HeRepository.kt
+++ app/src/main/java/me/spica/weather/repository/HeRepository.kt
@@ -1,16 +1,19 @@
 package me.spica.weather.repository
 
 import android.content.Context
+import com.qweather.sdk.bean.weather.WeatherDailyBean
+import com.qweather.sdk.bean.weather.WeatherHourlyBean
 import com.qweather.sdk.bean.weather.WeatherNowBean
-import com.qweather.sdk.view.HeContext
 import com.qweather.sdk.view.QWeather
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.trySendBlocking
-import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.withContext
-import me.spica.weather.model.weather.NowWeatherBean
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
+import me.spica.weather.model.weather.toDailyWeatherBean
+import me.spica.weather.model.weather.toHourlyWeatherBean
 import me.spica.weather.model.weather.toNowWeatherBean
-import java.util.concurrent.Flow
 
 
 /**
@@ -44,6 +47,65 @@ class HeRepository(private val context: Context) : Repository {
             }
         )
 
+    }.onStart {
+        onStart()
+    }.onCompletion {
+        onComplete()
+    }.flowOn(Dispatchers.IO)
+
+    override fun fetchHourlyWeather(
+        lon: String,
+        lat: String,
+        onStart: () -> Unit,
+        onComplete: () -> Unit,
+        onError: (String?) -> Unit,
+        onSuccess: () -> Unit
+    ) = callbackFlow {
+        QWeather.getWeather24Hourly(context,
+            "${lon},${lat}",
+            object : QWeather.OnResultWeatherHourlyListener {
+                override fun onError(error: Throwable) {
+                    onError(error.message)
+                    close(error)
+                }
+
+                override fun onSuccess(result: WeatherHourlyBean) {
+                    trySendBlocking(result.hourly.map {
+                        it.toHourlyWeatherBean()
+                    })
+                }
+
+            })
+    }.onStart {
+        onStart()
+    }.onCompletion {
+        onComplete()
+    }.flowOn(Dispatchers.IO)
+
+
+    override fun fetchDailyWeather(
+        lon: String,
+        lat: String,
+        onStart: () -> Unit,
+        onComplete: () -> Unit,
+        onError: (String?) -> Unit,
+        onSuccess: () -> Unit
+    ) = callbackFlow {
+        QWeather.getWeather7D(context, "${lon},${lat}",
+            object : QWeather.OnResultWeatherDailyListener {
+                override fun onError(error: Throwable) {
+                    close(error)
+                    onError(error.message)
+                }
+
+                override fun onSuccess(result: WeatherDailyBean) {
+                    trySendBlocking(result.daily.map {
+                        it.toDailyWeatherBean()
+                    })
+                }
+
+            }
+        )
     }.onStart {
         onStart()
     }.onCompletion {
--- app/src/main/java/me/spica/weather/repository/Repository.kt
+++ app/src/main/java/me/spica/weather/repository/Repository.kt
@@ -1,5 +1,7 @@
 package me.spica.weather.repository
 
+import me.spica.weather.model.weather.DailyWeatherBean
+import me.spica.weather.model.weather.HourlyWeatherBean
 import me.spica.weather.model.weather.NowWeatherBean
 
 interface Repository {
@@ -14,6 +16,29 @@ interface Repository {
         onError: (String?) -> Unit,
         onSuccess: () -> Unit
     ): kotlinx.coroutines.flow.Flow<NowWeatherBean>
+
+
+    // 获取近几小时的天气
+    fun fetchHourlyWeather(
+        lon: String,
+        lat: String,
+        onStart: () -> Unit,
+        onComplete: () -> Unit,
+        onError: (String?) -> Unit,
+        onSuccess: () -> Unit
+    ): kotlinx.coroutines.flow.Flow<List<HourlyWeatherBean>>
+
+
+
+    // 获取近几天的天气
+    fun fetchDailyWeather(
+        lon: String,
+        lat: String,
+        onStart: () -> Unit,
+        onComplete: () -> Unit,
+        onError: (String?) -> Unit,
+        onSuccess: () -> Unit
+    ): kotlinx.coroutines.flow.Flow<List<DailyWeatherBean>>
 
 
 }
