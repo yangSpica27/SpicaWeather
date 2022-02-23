92ce49b2feebf537eb228d5e5407c24bd24e88f9
spica27 <1286939382@qq.com>
2022-02-23 23:03:34 +0800

    # 调通百度地图

--- app/src/main/java/me/spica/weather/ui/main/MainActivity.kt
+++ app/src/main/java/me/spica/weather/ui/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package me.spica.weather.ui.main
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Rect
 import android.graphics.Typeface
 import android.net.Uri
@@ -12,11 +14,18 @@ import android.view.View
 import android.view.Window
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
+import com.baidu.location.BDAbstractLocationListener
+import com.baidu.location.BDLocation
+import com.baidu.location.LocationClient
+import com.baidu.location.LocationClientOption
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.transition.platform.MaterialContainerTransform
 import com.google.android.material.transition.platform.MaterialContainerTransformSharedElementCallback
+import com.yanzhenjie.permission.AndPermission
+import com.yanzhenjie.permission.runtime.Permission
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
@@ -30,9 +39,11 @@ import me.spica.weather.model.city.CityBean
 import me.spica.weather.tools.doOnMainThreadIdle
 import me.spica.weather.tools.dp
 import me.spica.weather.tools.keyboard.FluidContentResizer
+import me.spica.weather.tools.toast
 import me.spica.weather.ui.about.AboutActivity
 import me.spica.weather.ui.city.CitySelectActivity
 import me.spica.weather.ui.city.WeatherCityActivity
+import timber.log.Timber
 
 
 @AndroidEntryPoint
@@ -41,6 +52,16 @@ class MainActivity : BindingActivity<ActivityMainBinding>() {
 
     private val viewModel: WeatherViewModel by viewModels()
 
+    private var locationClint: LocationClient? = null
+
+
+    private val locationListener = object :BDAbstractLocationListener(){
+        override fun onReceiveLocation(result: BDLocation) {
+            Timber.i("BDMAP:${result.locType}")
+            toast(result.city)
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         window.requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS)
@@ -108,12 +129,49 @@ class MainActivity : BindingActivity<ActivityMainBinding>() {
     }
 
 
-
-
     override fun onResume() {
         super.onResume()
+        getLocation()
     }
 
+
+    private fun getLocation() {
+        if (hasPermissions()) {
+
+            if (locationClint == null) {
+                locationClint = LocationClient(this)
+                locationClint?.locOption = LocationClientOption().apply {
+                    setIsNeedAddress(true)
+                    setNeedNewVersionRgc(true)
+                    locationMode = LocationClientOption.LocationMode.Battery_Saving
+                }
+                locationClint?.registerLocationListener(locationListener)
+                locationClint?.start()
+            }
+
+        } else {
+            AndPermission.with(this)
+                .runtime()
+                .permission(Permission.ACCESS_FINE_LOCATION)
+                .permission(Permission.ACCESS_COARSE_LOCATION)
+                .permission(Permission.Group.LOCATION)
+                .onGranted {
+                    getLocation()
+                }
+                .start()
+        }
+    }
+
+
+    private fun hasPermissions(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
 
     private fun initView() {
         viewBinding.scrollView.post {
