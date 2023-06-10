846788bc5dd8358d8e8253d21ad699b28125cbd4
heze14 <1286939372@qq.com>
2023-06-10 13:39:58 +0800

    # try fix change city anim bug

--- app/src/main/java/me/spica/weather/ui/city/WeatherCityAdapter.kt
+++ app/src/main/java/me/spica/weather/ui/city/WeatherCityAdapter.kt
@@ -6,8 +6,6 @@ import android.app.ActivityOptions
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.AsyncListDiffer
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.airbnb.lottie.LottieAnimationView
 import me.spica.weather.R
@@ -24,78 +22,86 @@ import me.spica.weather.ui.main.MainActivity
  * 城市
  */
 class WeatherCityAdapter(
-  private val activity: Activity,
+    private val activity: Activity,
 ) : RecyclerView.Adapter<WeatherCityAdapter.ViewHolder>() {
 
-  val diffUtil = AsyncListDiffer(
-    this,
-    object : DiffUtil.ItemCallback<CityBean>() {
-      override fun areItemsTheSame(oldItem: CityBean, newItem: CityBean): Boolean = oldItem.cityName == newItem.cityName
-      override fun areContentsTheSame(oldItem: CityBean, newItem: CityBean): Boolean = false
+
+    val items: MutableList<CityBean> = arrayListOf()
+
+    init {
+        setHasStableIds(true)
     }
-  )
 
 
-  var itemClickListener: (CityBean) -> Unit = {}
-
-  class ViewHolder(val itemBinding: ItemWeatherCityBinding) :
-    RecyclerView.ViewHolder(itemBinding.root)
-
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-    val itemBinding = ItemWeatherCityBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-    return ViewHolder(itemBinding)
-  }
-
-  @SuppressLint("SetTextI18n", "ClickableViewAccessibility")
-  override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
-    // 代入文本
-    holder.itemBinding.tvCityName.text = diffUtil.currentList[position].cityName
-
-
-    val lottieAnimationView = holder.itemBinding.root.findViewById<LottieAnimationView>(R.id.lottie_view)
-    lottieAnimationView.setMaxProgress(.5f)
-    lottieAnimationView.setAnimation(
-      WeatherCodeUtils.getWeatherCode(
-        diffUtil.currentList[position].iconId
-      ).getAnimRes()
-    )
-    holder.itemBinding.tvLocation.text = "东经${diffUtil.currentList[position].lon}° | 北纬${diffUtil.currentList[position].lat}°"
-    if (diffUtil.currentList[position].isSelected) {
-      holder.itemBinding.icSelected.show()
-      holder.itemBinding.root.setOnTouchListener { _, _ ->
-        // 不可操作目前选中的item
-        return@setOnTouchListener true
-      }
-    } else {
-      holder.itemBinding.root.setOnTouchListener(null)
-      holder.itemBinding.icSelected.hide()
+    @SuppressLint("NotifyDataSetChanged")
+    fun notifyDataChanged(cities: List<CityBean>) {
+        items.clear()
+        items.addAll(cities)
+        notifyDataSetChanged()
     }
-    holder.itemView.rootView.setOnClickListener {
-      if (diffUtil.currentList[position].isSelected) {
-        holder.itemBinding.icSelected.hide()
-      }
-      itemClickListener(diffUtil.currentList[position])
-      holder.itemBinding.root.postDelayed(
-        {
-          doOnMainThreadIdle({
-            // 进入主页
-            val intent = Intent(activity, MainActivity::class.java)
-            val options = ActivityOptions.makeSceneTransitionAnimation(
-              activity,
-              holder.itemBinding.root,
-              "shared_element_container"
+
+    override fun getItemId(position: Int): Long {
+        return "${items[position].isSelected}${items[position].isSelected}".hashCode().toLong()
+    }
+
+
+    var itemClickListener: (CityBean) -> Unit = {}
+
+    class ViewHolder(val itemBinding: ItemWeatherCityBinding) :
+        RecyclerView.ViewHolder(itemBinding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemBinding = ItemWeatherCityBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(itemBinding)
+    }
+
+    @SuppressLint("SetTextI18n", "ClickableViewAccessibility")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        // 代入文本
+        holder.itemBinding.tvCityName.text = items[position].cityName
+
+
+        val lottieAnimationView = holder.itemBinding.root.findViewById<LottieAnimationView>(R.id.lottie_view)
+        lottieAnimationView.setMaxProgress(.5f)
+        lottieAnimationView.setAnimation(
+            WeatherCodeUtils.getWeatherCode(
+                items[position].iconId
+            ).getAnimRes()
+        )
+        holder.itemBinding.tvLocation.text = "东经${items[position].lon}° | 北纬${items[position].lat}°"
+        if (items[position].isSelected) {
+            holder.itemBinding.icSelected.show()
+            holder.itemBinding.root.setOnTouchListener { _, _ ->
+                // 不可操作目前选中的item
+                return@setOnTouchListener true
+            }
+        } else {
+            holder.itemBinding.root.setOnTouchListener(null)
+            holder.itemBinding.icSelected.hide()
+        }
+        holder.itemView.rootView.setOnClickListener {
+            itemClickListener(items[position])
+            holder.itemBinding.root.postDelayed(
+                {
+                    doOnMainThreadIdle({
+                        // 进入主页
+                        val intent = Intent(activity, MainActivity::class.java)
+                        val options = ActivityOptions.makeSceneTransitionAnimation(
+                            activity,
+                            holder.itemBinding.root,
+                            "shared_element_container"
+                        )
+                        activity.startActivity(intent, options.toBundle())
+                    })
+                }, 500
             )
-            activity.startActivity(intent, options.toBundle())
-          })
-        }, 500
-      )
+        }
+
+
     }
 
-
-  }
-
-  override fun getItemCount(): Int = diffUtil.currentList.size
+    override fun getItemCount(): Int = items.size
 
 
 }
